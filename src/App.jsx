78cd745a3f226f61b5928047649b717001606461
Nischal Jain <89198650+Nis-chal-Jain@users.noreply.github.com>
2025-03-03 16:34:37 +0000

    updated the model;

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   const { transcript, listening, resetTranscript } = useSpeechRecognition();
   const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
   const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
